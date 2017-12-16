80bfec0735aadc255bf2a282fb0651b84fb114fe
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-16 23:17:19 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+      "#{self.first_name} #{self.last_name}"
+    end
+    def list_roles
+      list_of_roles = []
+      self.characters.each do |c|
+        show = Show.where(id:c.show_id).pluck(:name)[0]
+        list_of_roles << "#{c.name} - #{show}"
+        #binding.pry
+      end
+      list_of_roles
+    end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+  def build_network(hash)
+    network_n = Network.find_or_create_by(hash)
+    self.network = network_n
+  end
 end
