dde446fcfb46056c8d0fae7e69d2639226509912
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-16 21:48:52 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,8 +1,8 @@
 class CreateCharacters < ActiveRecord::Migration[4.2]
-  def change 
+  def change
     create_table :characters do |c|
-      c.string :name 
+      c.string :name
       c.integer :show_id
-    end 
-  end 
+    end
+  end
 end 
