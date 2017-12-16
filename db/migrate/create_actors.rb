91656a7f8ee30c6969a4c891514877258ad7d50d
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-16 21:42:06 +0000

    automatically backed up by learn

--- db/migrate/create_actors.rb
+++ db/migrate/create_actors.rb
@@ -1,8 +1,8 @@
 class CreateActors < ActiveRecord::Migration[4.2]
-  def change 
+  def change
     create_table :actors do |a|
       a.string :first_name
       a.string :last_name
-    end 
-  end 
+    end
+  end
 end 
